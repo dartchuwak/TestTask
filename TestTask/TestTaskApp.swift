f1ebe5d4ea5b602ac770eeb2950f9d01ab0724cb
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-12-18 20:57:10 +0300

    Initial Commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Evgenii Mikhailov on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
